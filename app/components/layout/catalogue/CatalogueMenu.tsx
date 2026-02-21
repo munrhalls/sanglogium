fdb95261030d63005af5b56d7374e4db4ba80cdb
Munrhalls <antarcticdepths71@gmail.com>
2026-02-21 16:31:29 +0100

    feat(catalogue menu) - catalogue slot

--- app/components/layout/catalogue/CatalogueMenu.tsx
+++ app/components/layout/catalogue/CatalogueMenu.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { CatalogueItem } from "./data";
 import CatalogueHeader from "./CatalogueHeader";
+import { CatalogueSlot } from "./CatalogueSlot";
 
 export function CatalogueMenu({ data }: { data: CatalogueItem }) {
   const sections = data.sections;
@@ -18,33 +19,20 @@ export function CatalogueMenu({ data }: { data: CatalogueItem }) {
   return (
     <div className="flex h-full w-full items-start justify-center overflow-hidden bg-brand-700">
       <div className="flex h-full items-center lg:gap-12">
-        <div className="flex h-full flex-1 flex-col">
+        <div className="flex h-full flex-1 flex-col justify-center">
           {slots.map((_, i) => {
             const sectionIndex = activeSlots.indexOf(i);
             const section = sectionIndex !== -1 ? sections[sectionIndex] : null;
             const symmetryClass = SYMMETRY_STEPS[i] || "";
 
             return (
-              <div
+              // slot
+              <CatalogueSlot
                 key={i}
-                // ${symmetryClass}
-                className={`flex h-20 w-fit items-center justify-start text-right transition-transform duration-500`}
-              >
-                {section && (
-                  <div className="space-y-1">
-                    <h3 className="text-sm font-bold uppercase tracking-tighter text-brand-400">
-                      {section.title}
-                    </h3>
-                    <ul className="hidden md:block">
-                      {section.links.slice(0, 2).map((link) => (
-                        <li key={link} className="text-xs text-secondary-300">
-                          {link}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                )}
-              </div>
+                data={section}
+                index={i}
+                symmetryClass={symmetryClass}
+              />
             );
           })}
         </div>
