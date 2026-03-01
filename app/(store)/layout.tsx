6893c740d8805c7980433ddc0dda9ca81682b335
Munrhalls <antarcticdepths71@gmail.com>
2026-03-01 17:07:52 +0100

    fix(mobile header, mobile menu) - update global css vars to 44px, update main pt to use mobile menu h

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           <NuqsAdapter>
             <Header />
             <CatalogueNavbar />
-            <main className="relative flex w-full flex-1 flex-col pb-[var(--mobile-menu-h)] pt-[var(--header-h)]">
+            <main className="relative flex w-full flex-1 flex-col pb-[var(--mobile-menu-h)] pt-[var(--mobile-header-h)]">
               {children}
             </main>
             <Footer />
--- app/components/layout/drawers/DrawersManager.tsx
+++ app/components/layout/drawers/DrawersManager.tsx
@@ -15,19 +15,11 @@ export default function DrawerManager() {
     >
       <Drawer.Portal>
         <Drawer.Overlay
-          className="fixed inset-0 z-50 bg-black/10"
-          style={{
-            top: 'var(--header-h)',
-            bottom: 'var(--mobile-menu-h)'
-          }}
+          className="fixed inset-0 z-50 bg-black/10 top-[var(--mobile-header-h)] bottom-[var(--mobile-menu-h)]"
         />
 
         <Drawer.Content
-          className="fixed right-0 z-50 flex w-full outline-none lg:w-1/4"
-          style={{
-            top: 'var(--header-h)',
-            bottom: 'var(--mobile-menu-h)'
-          }}
+          className="fixed right-0 z-50 flex w-full outline-none lg:w-1/4 top-[var(--mobile-header-h)] bottom-[var(--mobile-menu-h)]"
         >
           <div className="flex w-full flex-col overflow-y-auto bg-brand-700 shadow-lg">
             <Drawer.Title className="sr-only">Drawer Content</Drawer.Title>
