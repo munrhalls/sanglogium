892c99757857dcb84c114e100d69471b6b6ae52f
Munrhalls <antarcticdepths71@gmail.com>
2026-03-06 09:56:29 +0100

    feat(hero) - use sanity hotspot crop data - delete code gymnastics - apply the hotspot data directly

--- app/components/features/homepage/hero/Hero.tsx
+++ app/components/features/homepage/hero/Hero.tsx
@@ -9,16 +9,13 @@ export default async function Hero() {
   if (!data?.backgroundImage || !data?.headline) {
     return null;
   }
+  const mobileBackgroundImage = data.mobileBackgroundImage || data.backgroundImage;
 
-  const { hotspot: desktopHotspot } = data.backgroundImage;
-  const desktopX = desktopHotspot?.x ?? 0.5;
-  const desktopY = desktopHotspot?.y ?? 0.5;
-
-  const mobileBackgroundImage =
-    data.mobileBackgroundImage || data.backgroundImage;
-  const { hotspot: mobileHotspot } = mobileBackgroundImage;
-  const mobileX = mobileHotspot?.x ?? 0.5;
-  const mobileY = mobileHotspot?.y ? mobileHotspot.y + 0.5 : 0.95;
+  const getPosition = (image: any) => {
+    const x = image.hotspot?.x ? image.hotspot.x * 100 : 50;
+    const y = image.hotspot?.y ? image.hotspot.y * 100 : 50;
+    return `${x}% ${y}%`;
+  };
 
   return (
     <section
@@ -38,9 +35,7 @@ export default async function Hero() {
           className={cn("block object-cover", "md:hidden")}
           sizes="100vw"
           quality={90}
-          style={{
-            objectPosition: `${mobileX * 100}% ${mobileY * 100}%`,
-          }}
+          style={{ objectPosition: getPosition(mobileBackgroundImage) }}
         />
 
         {/* IMAGE 2: DESKTOP (Visible >= md) */}
@@ -52,9 +47,7 @@ export default async function Hero() {
           className={cn("hidden object-cover", "md:block")}
           sizes="100vw"
           quality={90}
-          style={{
-            objectPosition: `${desktopX * 100}% ${desktopY * 100}%`,
-          }}
+          style={{ objectPosition: getPosition(data.backgroundImage) }}
         />
 
         <div
