900b4d9c3d053ab04c2484c1b4320357cc3f8ade
Munrhalls <antarcticdepths71@gmail.com>
2026-03-07 15:22:14 +0100

    style(layout): normalize Shelf to .20 structural scale (80px py)

--- app/components/layout/general/Shelf.tsx
+++ app/components/layout/general/Shelf.tsx
@@ -12,11 +12,8 @@ export default function Shelf({ children, className, id }: ShelfProps) {
       id={id}
       className={cn(
         "w-full",
-
-        "py-20 md:py-36 landscape:py-10 md:landscape:py-36",
-
+        "py-20", 
         "px-6 md:px-12",
-        "outline-1 outline-dashed outline-white/20 -outline-offset-1",
         className
       )}
     >
