bae2d03683073d5b80f36a4a1736b17fe60e8510
Munrhalls <antarcticdepths71@gmail.com>
2026-03-09 17:28:46 +0100

    feat(newest-release): establish SpotlightRelease data contract

---
+++ app/components/features/homepage/newest-release/types.ts
@@ -0,0 +1,11 @@
+﻿export interface SpotlightRelease {
+  readonly _id: string;
+  readonly tag: string;
+  readonly name: string;
+  readonly brand: string;
+  readonly displayPrice: number;
+  readonly description: string;
+  readonly imageUrl: string;
+  readonly images: readonly string[];
+  readonly slug?: string;
+}
