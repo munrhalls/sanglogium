dc6f06dcf70618ea4bd5529062be52f67ae713aa
Munrhalls <antarcticdepths71@gmail.com>
2026-04-03 13:21:21 +0200

    Difficulty: 1 - D, Configuration (tailwind): update global design tokens — → closes 0 DoD items, infrastructure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -334,7 +334,7 @@ const uiComponentsPlugin = plugin(function ({ addComponents, theme }) {
       fontWeight: theme("fontWeight.regular") as string,
       color: theme("colors.text.body") as string,
       appearance: "none",
-      backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 20 20'%3e%3cpath stroke='%23${theme("colors.text.body").slice(1)}' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M6 8l4 4 4-4'/%3e%3c/svg%3e")`,
+      backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 20 20'%3e%3cpath stroke='%23${(theme("colors.text.body") as string).slice(1)}' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M6 8l4 4 4-4'/%3e%3c/svg%3e")`,
       backgroundPosition: `right ${theme("spacing.3")} center`,
       backgroundRepeat: "no-repeat",
       backgroundSize: "1.5em 1.5em",
