a7b201ecd399ab0b8fd399619a650ed711ac139c
Munrhalls <antarcticdepths71@gmail.com>
2026-04-18 13:49:12 +0200

    Difficulty: 3 - A, Test Infrastructure (app/queue-skeleton-e2e-test-page): Implement concurrent checkout queue E2E test page — → closes DoD item on checkout queue skeleton

--- app/queue-skeleton-e2e-test-page/layout.tsx
+++ app/queue-skeleton-e2e-test-page/layout.tsx
@@ -1,1 +1,2 @@
 export { default } from '@/tests/checkout-queue-skeleton/e2e/queue-skeleton-e2e-test-page/layout'
+export { metadata } from '@/tests/checkout-queue-skeleton/e2e/queue-skeleton-e2e-test-page/layout'
