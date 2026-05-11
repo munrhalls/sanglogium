444a31c5087495dd30ff4d7616c91b85f5f5d0bd
Munrhalls <antarcticdepths71@gmail.com>
2026-05-11 15:40:25 +0200

    Difficulty: 3 - A, Types (checkout): Add checkout types definition file → DoD:0 <infrastructure>

---
+++ app/(store)/checkout/checkout.types.ts
@@ -0,0 +1,36 @@
+export type Address = {
+  regionCode: string;
+  postalCode: string;
+  street: string;
+  streetNumber: string;
+  city: string;
+};
+
+export type Status = "LOADING" | "FIX" | "CONFIRM" | "ACCEPT";
+
+export type ServerResponse = {
+  status: Status;
+  address?: Address;
+  geocode?: {
+    location: {
+      latitude: number;
+      longitude: number;
+    };
+  };
+  placeId?: string;
+  errors?: Record<string, string>;
+};
+
+export type ServerProduct = {
+  _id: string;
+  name: string;
+  price: number;
+  stock: number;
+  stripePriceId: string;
+  _rev: string;
+};
+
+export type BasketCheckoutItem = {
+  _id: string;
+  quantity: number;
+};
