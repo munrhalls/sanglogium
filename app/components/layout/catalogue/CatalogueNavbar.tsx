f4281ede37ebb1038098ad0d27b1498673f632a1
Munrhalls <antarcticdepths71@gmail.com>
2026-06-18 14:52:43 +0200

    Difficulty: 1 - C, CSS (catalogue navbar): Standardize lg-desktop to lg class → DoD:0

--- app/components/layout/catalogue/CatalogueNavbar.tsx
+++ app/components/layout/catalogue/CatalogueNavbar.tsx
@@ -20,7 +20,7 @@ const CatalogueNavbar = async ({ catalogueDataRaw }: CatalogueNavbarProps) => {
   return (
     <nav
       className={cn(
-        "hidden w-full shrink-0 items-center bg-brand-900 lg:flex lg-desktop:h-[var(--desktop-catalogue-nav-h)]"
+        "hidden w-full shrink-0 items-center bg-brand-900 lg:flex lg:h-[var(--desktop-catalogue-nav-h)]"
       )}
       aria-label="Catalogue Navigation"
     >
