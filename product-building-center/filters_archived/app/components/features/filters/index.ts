f4f3026a82d26b1978b61ae3ba83be27fd5a6b69
Munrhalls <antarcticdepths71@gmail.com>
2026-08-26 22:37:49 +0200

    Difficulty: 5 - C, Refactor (catalogue filters-sorting, filters_archived, filter docs, Sanity filter modules): remove legacy filters/sorting system, its archived copy, filter/search docs and Sanity filter query modules -> DoD:0 infrastructure/deferred

--- product-building-center/filters_archived/app/components/features/filters/index.ts
+++
@@ -1,6 +0,0 @@
-export { FilterSidebar } from './FilterSidebar';
-export { SortDropdown } from './SortDropdown';
-export { ActiveFilters } from './ActiveFilters';
-export { MobileFilterDrawer } from './MobileFilterDrawer';
-export { MobileControlsBar } from './MobileControlsBar';
-export { useFilterNuqs } from './useFilterNuqs';
